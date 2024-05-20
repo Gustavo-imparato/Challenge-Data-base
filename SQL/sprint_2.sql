91de95af516e174b3947047cfbc367f791d6b616
Gustavo-imparato <129402642+Gustavo-imparato@users.noreply.github.com>
2024-05-20 18:24:20 -0300

    Update sprint_2.sql
    versao final

--- SQL/sprint_2.sql
+++ SQL/sprint_2.sql
@@ -236,17 +236,18 @@ EXCEPTION
 END;
 
 --UPDATE EMPRESA
-
-CREATE OR REPLACE PROCEDURE     CRIAR_EMPRESA(
+CREATE OR REPLACE PROCEDURE CRIAR_EMPRESA(
     p_id_empresa IN NUMBER,
     p_nm_empresa IN VARCHAR2,
-    p_descricao_empresa IN VARCHAR2
+    p_razao_social IN VARCHAR2,
+    p_nome_fantasia IN VARCHAR2
 )
 AS 
 BEGIN
-    UPDATE tb_empresa
+    UPDATE tb_empresa1
     SET nm_empresa = p_nm_empresa,
-        descricao_empresa = p_descricao_empresa
+        razao_social = p_razao_social,
+        nome_fantasia = p_nome_fantasia
     WHERE id_empresa = p_id_empresa;
     
     dbms_output.put_line('Update realizado com sucesso em empresa!');
@@ -258,14 +259,18 @@ END;
 --UPDATE USUARIO 
 CREATE OR REPLACE PROCEDURE CRIAR_USUARIO(
     p_id_usuario IN NUMBER,
+    p_nome IN VARCHAR2,
     p_nm_usuario IN VARCHAR2,
-    p_descricao_usuario IN VARCHAR2
+    p_senha IN VARCHAR2,
+    p_id_empresa IN NUMBER
 )
 AS 
 BEGIN
-    UPDATE tb_usuario
-    SET nm_usuario = p_nm_usuario,
-        descricao_usuario = p_descricao_usuario
+    UPDATE tb_usuario1
+    SET nome = p_nome,
+        nm_usuario = p_nm_usuario,
+        senha = p_senha,
+        id_empresa = p_id_empresa
     WHERE id_usuario = p_id_usuario;
     
     dbms_output.put_line('Update realizado com sucesso em usuário!');
@@ -277,21 +282,26 @@ END;
 --UPDATE BACKLOG
 CREATE OR REPLACE PROCEDURE CRIAR_BACKLOG(
     p_id_backlog IN NUMBER,
-    p_nm_backlog IN VARCHAR2,
-    p_descricao_backlog IN VARCHAR2
+    p_titulo IN VARCHAR2,
+    p_descricao_backlog IN VARCHAR2,
+    p_id_usuario IN NUMBER,
+    p_id_empresa IN NUMBER
 )
 AS 
 BEGIN
-    UPDATE tb_backlog
-    SET nm_backlog = p_nm_backlog,
-        descricao_backlog = p_descricao_backlog
+    UPDATE tb_backlog1
+    SET titulo = p_titulo,
+        descricao_backlog = p_descricao_backlog,
+        id_usuario = p_id_usuario,
+        id_empresa = p_id_empresa
     WHERE id_backlog = p_id_backlog;
     
-    dbms_output.put_line('update realizado com sucesso em backlog!');
+    dbms_output.put_line('Update realizado com sucesso em backlog!');
 EXCEPTION
     WHEN OTHERS THEN
         dbms_output.put_line('Erro no update de backlog: ' || SQLERRM);
 END;
+
 
 
 --PROCEDURE DELETE
@@ -304,7 +314,7 @@ IS
 BEGIN
 
     IF id_marca IS NULL OR id_marca = 0 THEN
-        RAISE_APPLICATION_ERROR(-20000, 'ID da marca inválido: nulo ou zero!');
+        RAISE_APPLICATION_ERROR(-20000, 'ID da marca invÃ¡lido: nulo ou zero!');
     END IF;
 
     DELETE FROM tb_marca1
@@ -326,7 +336,7 @@ IS
 BEGIN
 
     IF id_categoria IS NULL OR id_categoria = 0 THEN
-        RAISE_APPLICATION_ERROR(-20000, 'ID da categoria inválido: nulo ou zero!');
+        RAISE_APPLICATION_ERROR(-20000, 'ID da categoria invÃ¡lido: nulo ou zero!');
     END IF;
 
     DELETE FROM tb_categoria1
@@ -348,7 +358,7 @@ IS
 BEGIN
 
     IF id_modelo IS NULL OR id_modelo = 0 THEN
-        RAISE_APPLICATION_ERROR(-20000, 'ID do modelo inválido: nulo ou zero!');
+        RAISE_APPLICATION_ERROR(-20000, 'ID do modelo invÃ¡lido: nulo ou zero!');
     END IF;
 
     DELETE FROM tb_modelo1
@@ -370,7 +380,7 @@ IS
 BEGIN
 
     IF id_venda IS NULL OR id_venda = 0 THEN
-        RAISE_APPLICATION_ERROR(-20000, 'ID da venda inválido: nulo ou zero!');
+        RAISE_APPLICATION_ERROR(-20000, 'ID da venda invÃ¡lido: nulo ou zero!');
     END IF;
 
     DELETE FROM tb_historico_venda1
@@ -381,7 +391,7 @@ BEGIN
 EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erro ao deletar venda: ' || SQLERRM);
-END deletar_deletar_venda;
+END deletar_historico_venda;
 
 
 --DELETE PRODUTO
@@ -392,7 +402,7 @@ IS
 BEGIN
 
     IF id_produto IS NULL OR id_produto = 0 THEN
-        RAISE_APPLICATION_ERROR(-20000, 'ID do produto inválido: nulo ou zero!');
+        RAISE_APPLICATION_ERROR(-20000, 'ID do produto invÃ¡lido: nulo ou zero!');
     END IF;
 
     DELETE FROM tb_produto1
@@ -413,7 +423,7 @@ IS
 BEGIN
 
     IF id_carrinho IS NULL OR id_carrinho = 0 THEN
-        RAISE_APPLICATION_ERROR(-20000, 'ID do carrinho inválido: nulo ou zero!');
+        RAISE_APPLICATION_ERROR(-20000, 'ID do carrinho invÃ¡lido: nulo ou zero!');
     END IF;
 
     DELETE FROM tb_carrinho1
@@ -428,75 +438,77 @@ END deletar_carrinho;
 
 --DELETE EMPRESA 
 CREATE OR REPLACE PROCEDURE deletar_empresa (
-   id_empresa IN NUMBER
+   p_id_empresa IN NUMBER
 )
 IS
 BEGIN
-
-    IF id_empresa IS NULL OR id_empresa = 0 THEN
-        RAISE_APPLICATION_ERROR(-20001, 'ID da empresa inválido: nulo ou zero!');
+    IF p_id_empresa IS NULL OR p_id_empresa = 0 THEN
+        RAISE_APPLICATION_ERROR(-20000, 'ID da empresa inválido: nulo ou zero!');
     END IF;
 
-    DELETE FROM tb_empresa
-    WHERE id_empresa = id_empresa;
+    DELETE FROM tb_empresa1
+    WHERE id_empresa = p_id_empresa;
 
     -- Mensagem de sucesso
-    DBMS_OUTPUT.PUT_LINE('Empresa com ID ' || id_empresa || ' deletada com sucesso!');
+    DBMS_OUTPUT.PUT_LINE('Empresa com ID ' || p_id_empresa || ' deletada com sucesso!');
 EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erro ao deletar empresa: ' || SQLERRM);
 END deletar_empresa;
 
+
 --DELETAR USUARIO 
 CREATE OR REPLACE PROCEDURE deletar_usuario (
-   id_usuario IN NUMBER
+   p_id_usuario IN NUMBER
 )
 IS
 BEGIN
-
-    IF id_usuario IS NULL OR id_usuario = 0 THEN
-        RAISE_APPLICATION_ERROR(-20002, 'ID do usuário inválido: nulo ou zero!');
+    IF p_id_usuario IS NULL OR p_id_usuario = 0 THEN
+        RAISE_APPLICATION_ERROR(-20000, 'ID do usuário inválido: nulo ou zero!');
     END IF;
 
-    DELETE FROM tb_usuario
-    WHERE id_usuario = id_usuario;
+    DELETE FROM tb_usuario1
+    WHERE id_usuario = p_id_usuario;
 
     -- Mensagem de sucesso
-    DBMS_OUTPUT.PUT_LINE('Usuário com ID ' || id_usuario || ' deletado com sucesso!');
+    DBMS_OUTPUT.PUT_LINE('Usuário com ID ' || p_id_usuario || ' deletado com sucesso!');
 EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erro ao deletar usuário: ' || SQLERRM);
 END deletar_usuario;
 
+
 --DELETE BACKLOG
 CREATE OR REPLACE PROCEDURE deletar_backlog (
-   id_backlog IN NUMBER
+   p_id_backlog IN NUMBER
 )
 IS
 BEGIN
-
-    IF id_backlog IS NULL OR id_backlog = 0 THEN
-        RAISE_APPLICATION_ERROR(-20003, 'ID do backlog inválido: nulo ou zero!');
+    IF p_id_backlog IS NULL OR p_id_backlog = 0 THEN
+        RAISE_APPLICATION_ERROR(-20000, 'ID do backlog inválido: nulo ou zero!');
     END IF;
 
-    DELETE FROM tb_backlog
-    WHERE id_backlog = id_backlog;
+    DELETE FROM tb_backlog1
+    WHERE id_backlog = p_id_backlog;
 
     -- Mensagem de sucesso
-    DBMS_OUTPUT.PUT_LINE('Backlog com ID ' || id_backlog || ' deletado com sucesso!');
+    DBMS_OUTPUT.PUT_LINE('Backlog com ID ' || p_id_backlog || ' deletado com sucesso!');
 EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erro ao deletar backlog: ' || SQLERRM);
 END deletar_backlog;
 
+--PROCEDURES COM CURSOR JOIN
 
+--PRIMEIRO
+set serveroutput on;
 DECLARE
     v_id_carrinho tb_carrinho1.id_carrinho%TYPE;
     v_qnt_produtos tb_carrinho1.qnt_produtos%TYPE;
     v_sub_total tb_carrinho1.sub_total%TYPE;
     v_id_venda tb_historico_venda1.id_venda%TYPE;
 BEGIN
-    DBMS_OUTPUT.PUT_LINE('Carrinho e Histórico de Vendas:');
+    DBMS_OUTPUT.PUT_LINE('Carrinho e Historico de Vendas:');
     
     FOR i IN (SELECT c.id_carrinho, c.qnt_produtos, c.sub_total, h.id_venda
                             FROM tb_carrinho1 c
@@ -511,6 +523,47 @@ BEGIN
     END LOOP;
 END;
 
+
+--SEGUNDO
+set serveroutput on;
+CREATE OR REPLACE PROCEDURE listar_empresa_backlog IS
+    v_id_empresa tb_empresa1.id_empresa%TYPE;
+    v_nm_empresa tb_empresa1.nm_empresa%TYPE;
+    v_razao_social tb_empresa1.razao_social%TYPE;
+    v_nome_fantasia tb_empresa1.nome_fantasia%TYPE;
+    v_id_backlog tb_backlog1.id_backlog%TYPE;
+    v_titulo tb_backlog1.titulo%TYPE;
+    v_descricao_backlog tb_backlog1.descricao_backlog%TYPE;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Empresas e seus Backlogs:');
+    
+    FOR i IN (
+        SELECT e.id_empresa, e.nm_empresa, e.razao_social, e.nome_fantasia,
+               b.id_backlog, b.titulo, b.descricao_backlog
+        FROM tb_empresa1 e
+        JOIN tb_backlog1 b ON e.id_empresa = b.id_empresa
+    )
+    LOOP
+        v_id_empresa := i.id_empresa;
+        v_nm_empresa := i.nm_empresa;
+        v_razao_social := i.razao_social;
+        v_nome_fantasia := i.nome_fantasia;
+        v_id_backlog := i.id_backlog;
+        v_titulo := i.titulo;
+        v_descricao_backlog := i.descricao_backlog;
+        
+        DBMS_OUTPUT.PUT_LINE('ID Empresa: ' || v_id_empresa || ', Nome Empresa: ' || v_nm_empresa || 
+                             ', Razão Social: ' || v_razao_social || ', Nome Fantasia: ' || v_nome_fantasia ||
+                             ', ID Backlog: ' || v_id_backlog || ', Título: ' || v_titulo || 
+                             ', Descrição: ' || v_descricao_backlog);
+    END LOOP;
+END listar_empresa_backlog;
+
+
+
+
+--IMPLEMENTACAO DA REGRA DE NEGOCIO
+--CRUZANDO A TABELA HISTORICO VENDA E PRODUTO
 CREATE OR REPLACE PROCEDURE cruzar_produto_venda AS
 BEGIN
     FOR produto_venda IN (
@@ -536,8 +589,8 @@ BEGIN
         
         DBMS_OUTPUT.PUT_LINE(
             'Produto: ' || produto_venda.nm_produto || 
-            ', Preço de Custo: ' || produto_venda.preco_custo || 
-            ', Preço de Venda: ' || produto_venda.preco_venda || 
+            ', PreÃ§o de Custo: ' || produto_venda.preco_custo || 
+            ', PreÃ§o de Venda: ' || produto_venda.preco_venda || 
             ', ID Venda: ' || produto_venda.id_venda || 
             ', Valor da Venda: ' || produto_venda.vl_venda || 
             ', Data da Venda: ' || produto_venda.dt_venda
@@ -550,7 +603,7 @@ DECLARE
 BEGIN
     DBMS_OUTPUT.PUT_LINE('Lista de produtos e seus precos:');
     cruzar_produto_venda;
-    DBMS_OUTPUT.PUT_LINE('A consulta de precos foi concluída com sucesso.');
+    DBMS_OUTPUT.PUT_LINE('A consulta de precos foi concluÃ­da com sucesso.');
 EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Ocorreu um erro ao executar a consulta: ' || SQLERRM);
